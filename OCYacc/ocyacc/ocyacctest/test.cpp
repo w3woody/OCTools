5998ca2487d9593e1c97241f966ea7ef94934326
William Woody <woody@alumni.caltech.edu>
2017-07-25 22:14:44 -0400

    Add yacc compiler test

---
+++ OCYacc/ocyacc/ocyacctest/test.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  ocyacctest
+//
+//  Created by William Woody on 7/25/17.
+//  Copyright © 2017 Glenview Software. All rights reserved.
+//
+
+#include <iostream>
+#include "OCYaccParser.h"
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
