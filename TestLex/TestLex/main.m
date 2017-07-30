2425d69ff91f7a6b97c3d832cb5dcabed1d793eb
William Woody <woody@alumni.caltech.edu>
2017-07-30 15:54:52 -0400

    Test harness

--- TestLex/TestLex/main.m
+++ TestLex/TestLex/main.m
@@ -7,11 +7,35 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "OCLexer.h"
+
+@interface TestReader : NSObject <OCFileInput>
+@property (assign) NSInteger pos;
+@property (copy) NSString *test;
+@end
+
+@implementation TestReader
+- (int)readByte
+{
+	if (self.pos >= self.test.length) return -1;
+	return [self.test characterAtIndex:_pos++];
+}
+@end
+
 
 int main(int argc, const char * argv[]) {
 	@autoreleasepool {
 	    // insert code here...
-	    NSLog(@"Hello, World!");
+
+		TestReader *r = [[TestReader alloc] init];
+		r.test = @"132 . A123 0x0FG";
+
+		OCLexer *lexer = [[OCLexer alloc] initWithStream:r];
+		NSInteger ident;
+
+		while (-1 != (ident = [lexer lex])) {
+			NSLog(@"-> %ld %@\n",(long)ident,lexer.text);
+		}
 	}
 	return 0;
 }
