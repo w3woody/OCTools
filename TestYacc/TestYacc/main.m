ffd49b84a5d5919824910c7bae805f40bfdd11b8
William Woody <woody@alumni.caltech.edu>
2017-08-03 08:57:47 -0400

    Start framework for building yacc files

---
+++ TestYacc/TestYacc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestYacc
+//
+//  Created by William Woody on 8/3/17.
+//  Copyright © 2017 Glenview Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
